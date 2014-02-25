439fa0031390982601ab086ac3a2753802aa169f
Michael Young <michaeljy@gmail.com>
2014-02-24 19:40:26 -0600

    tests for title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,41 +1,43 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
 
   describe "Home page" do
+
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      expect(page).to have_content('Sample App')
     end
 
     it "should have the right title" do
       visit '/static_pages/home'
-      page.has_title?('Home')
+      expect(page).to have_title('RoR Sample App | Home')
     end
   end
 
   describe "Help page" do
+
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
       visit '/static_pages/help'
-      page.has_title?('Help')
+      expect(page).to have_title('RoR Sample App | Help')
     end
   end
 
   describe "About page" do
+
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      expect(page).to have_content('About Us')
     end
 
     it "should have the right title" do
       visit '/static_pages/about'
-      page.has_title?('About Us')
+      expect(page).to have_title('RoR Sample App | About Us')
     end
   end
-
 end
